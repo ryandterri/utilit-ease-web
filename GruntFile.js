7e5e3e3de700978370697b9ecebebbb17ae4a3ee
Ryan Purcell <patrick.ryan.purcell@gmail.com>
2019-01-02 23:26:11 -0600

    Added default env.js file and removed unneeded libraries and grunt tasks

--- GruntFile.js
+++ GruntFile.js
@@ -1,7 +1,5 @@
 module.exports = function (grunt) {
 
-    const build_number = grunt.option('build_number') || 'local';
-
     grunt.initConfig({
         copy: {
             views: {
@@ -11,29 +9,12 @@ module.exports = function (grunt) {
                     src: ['views/**/*.html'],
                     dest: 'client/'
                 }]
-            },
-            fonts: {
-                files: [{
-                    expand: true,
-                    cwd: 'node_modules/bootstrap/',
-                    src: ['fonts/**'],
-                    dest: 'client/'
-                }]
-            },
-            webfonts: {
-                files: [{
-                    expand: true,
-                    cwd: 'node_modules/@fortawesome/fontawesome-free-webfonts/',
-                    src: ['webfonts/**'],
-                    dest: 'client/'
-                }]
             }
         },
         concat: {
             core: {
                 src: [
                     'node_modules/jquery/dist/jquery.js',
-                    'node_modules/bootstrap/dist/js/bootstrap.js',
                     'node_modules/angular/angular.js',
                     'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
                     'node_modules/underscore/underscore.js',
@@ -49,36 +30,15 @@ module.exports = function (grunt) {
                 dest: 'client/js/utilit-ease.js'
             }
         },
-        less: {
-            development: {
-                files: {
-                    'src/css/bootstrap.css': 'node_modules/bootstrap/less/bootstrap.less',
-                    'src/css/font-awesome.css': [
-                        'node_modules/@fortawesome/fontawesome-free-webfonts/css/fontawesome.css',
-                        'node_modules/@fortawesome/fontawesome-free-webfonts/css/fa-brands.css',
-                        'node_modules/@fortawesome/fontawesome-free-webfonts/css/fa-regular.css',
-                        'node_modules/@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
-                    ]
-                }
-            }
-        },
         uglify: {
             development: {
                 files: {
                     'client/js/site.min.js': ['client/js/site.js']
                 }
             }
-        },
-        cssmin: {
-            development: {
-                files: {
-                    'client/css/site.min.css': ['src/css/*.css']
-                }
-            }
         }
     });
 
     require('load-grunt-tasks')(grunt);
-    grunt.registerTask('build', ['copy', 'concat', 'uglify', 'less', 'cssmin']);
-    grunt.registerTask('angular-build', ['copy', 'concat', 'uglify']);
+    grunt.registerTask('build', ['copy', 'concat', 'uglify']);
 };
---
+++ env.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+    // API url
+    window.__env.api_url = 'http://192.168.86.24:31219';
+}(this));
